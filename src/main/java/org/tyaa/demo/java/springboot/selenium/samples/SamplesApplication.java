0ee0bf2a1a69d58ccf9ad448b68cd07fad3817ed
YuriiTrofimenko <tyaa@ukr.net>
2021-07-09 22:02:34 +0300

    init

---
+++ src/main/java/org/tyaa/demo/java/springboot/selenium/samples/SamplesApplication.java
@@ -0,0 +1,13 @@
+package org.tyaa.demo.java.springboot.selenium.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tyaa/demo/java/springboot/selenium/samples/SamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tyaa.demo.java.springboot.selenium.samples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
